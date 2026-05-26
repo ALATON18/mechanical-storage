ce30874b96504a25cb9aab6d716689a977fd59b2
ALATON18 <littlexdoesminecraft@gmail.com>
2026-05-26 01:52:50 +0100

    Add mechanical storage terminal block

---
+++ src/main/java/com/mechanicalstorage/block/MechanicalStorageTerminalBlock.java
@@ -0,0 +1,7 @@
+package com.mechanicalstorage.block;
+
+public class MechanicalStorageTerminalBlock extends DirectionalMachineBlock {
+	public MechanicalStorageTerminalBlock(Properties properties) {
+		super(properties);
+	}
+}
