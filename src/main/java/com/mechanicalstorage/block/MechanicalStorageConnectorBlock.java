7aa37734d5b1cb7a96a263af99b9f80857a1324a
ALATON18 <littlexdoesminecraft@gmail.com>
2026-05-30 00:05:18 +0100

    Add connector right-click inventory status

--- src/main/java/com/mechanicalstorage/block/MechanicalStorageConnectorBlock.java
+++ src/main/java/com/mechanicalstorage/block/MechanicalStorageConnectorBlock.java
@@ -1,7 +1,40 @@
 package com.mechanicalstorage.block;
 
-public class MechanicalStorageConnectorBlock extends DirectionalMachineBlock {
+import com.mechanicalstorage.blockentity.MechanicalStorageConnectorBlockEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
+
+public class MechanicalStorageConnectorBlock extends DirectionalMachineBlock implements EntityBlock {
 	public MechanicalStorageConnectorBlock(Properties properties) {
 		super(properties);
+	}
+
+	@Nullable
+	@Override
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new MechanicalStorageConnectorBlockEntity(pos, state);
+	}
+
+	@Override
+	protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+		if (!level.isClientSide && player instanceof ServerPlayer serverPlayer && level.getBlockEntity(pos) instanceof MechanicalStorageConnectorBlockEntity connector) {
+			serverPlayer.sendSystemMessage(connector.describeTargetInventory());
+		}
+
+		return InteractionResult.SUCCESS;
+	}
+
+	@Override
+	protected InteractionResult useItemOn(net.minecraft.world.item.ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
+		return useWithoutItem(state, level, pos, player, hitResult);
 	}
 }
