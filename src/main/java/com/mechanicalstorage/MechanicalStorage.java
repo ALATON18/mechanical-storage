84d4b289791071e44712ff4e34f9b6bae6983ed4
ALATON18 <littlexdoesminecraft@gmail.com>
2026-05-30 00:06:02 +0100

    Register Mechanical Storage block entities

--- src/main/java/com/mechanicalstorage/MechanicalStorage.java
+++ src/main/java/com/mechanicalstorage/MechanicalStorage.java
@@ -2,13 +2,17 @@ package com.mechanicalstorage;
 
 import com.mechanicalstorage.block.MechanicalStorageConnectorBlock;
 import com.mechanicalstorage.block.MechanicalStorageTerminalBlock;
+import com.mechanicalstorage.blockentity.MechanicalStorageConnectorBlockEntity;
+import com.mechanicalstorage.blockentity.TerminalBlockEntity;
 import com.mojang.logging.LogUtils;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.neoforged.bus.api.IEventBus;
@@ -27,7 +31,8 @@ public class MechanicalStorage {
 
 	public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
 	public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
-	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(net.minecraft.core.registries.Registries.CREATIVE_MODE_TAB, MODID);
+	public static final DeferredRegister<BlockEntityType<?>> BLOCK_ENTITY_TYPES = DeferredRegister.create(Registries.BLOCK_ENTITY_TYPE, MODID);
+	public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
 	public static final DeferredBlock<Block> MECHANICAL_STORAGE_CONNECTOR = BLOCKS.register("mechanical_storage_connector", () ->
 			new MechanicalStorageConnectorBlock(machineProperties(MapColor.COLOR_GRAY)));
@@ -37,6 +42,12 @@ public class MechanicalStorage {
 
 	public static final DeferredItem<BlockItem> MECHANICAL_STORAGE_CONNECTOR_ITEM = ITEMS.registerSimpleBlockItem("mechanical_storage_connector", MECHANICAL_STORAGE_CONNECTOR);
 	public static final DeferredItem<BlockItem> MECHANICAL_STORAGE_TERMINAL_ITEM = ITEMS.registerSimpleBlockItem("mechanical_storage_terminal", MECHANICAL_STORAGE_TERMINAL);
+
+	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<MechanicalStorageConnectorBlockEntity>> MECHANICAL_STORAGE_CONNECTOR_BLOCK_ENTITY = BLOCK_ENTITY_TYPES.register("mechanical_storage_connector", () ->
+			BlockEntityType.Builder.of(MechanicalStorageConnectorBlockEntity::new, MECHANICAL_STORAGE_CONNECTOR.get()).build(null));
+
+	public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<TerminalBlockEntity>> MECHANICAL_STORAGE_TERMINAL_BLOCK_ENTITY = BLOCK_ENTITY_TYPES.register("mechanical_storage_terminal", () ->
+			BlockEntityType.Builder.of(TerminalBlockEntity::new, MECHANICAL_STORAGE_TERMINAL.get()).build(null));
 
 	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> MECHANICAL_STORAGE_TAB = CREATIVE_MODE_TABS.register("mechanical_storage", () -> CreativeModeTab.builder()
 			.title(Component.translatable("itemGroup.mechanical_storage"))
@@ -50,6 +61,7 @@ public class MechanicalStorage {
 	public MechanicalStorage(IEventBus modEventBus, ModContainer modContainer) {
 		BLOCKS.register(modEventBus);
 		ITEMS.register(modEventBus);
+		BLOCK_ENTITY_TYPES.register(modEventBus);
 		CREATIVE_MODE_TABS.register(modEventBus);
 
 		LOGGER.info("Mechanical Storage loaded");
