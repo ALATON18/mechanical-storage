82b55a2f49c4a6a8a42c12a814eaef3b868994bc
ALATON18 <littlexdoesminecraft@gmail.com>
2026-05-31 22:53:53 +0100

    Add terminal sort buttons and unfocused search

--- src/main/java/com/mechanicalstorage/client/TerminalScreen.java
+++ src/main/java/com/mechanicalstorage/client/TerminalScreen.java
@@ -2,6 +2,7 @@ package com.mechanicalstorage.client;
 
 import com.mechanicalstorage.menu.TerminalMenu;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
@@ -13,8 +14,8 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 	public TerminalScreen(TerminalMenu menu, Inventory playerInventory, Component title) {
 		super(menu, playerInventory, title);
 		this.imageWidth = 176;
-		this.imageHeight = 242;
-		this.inventoryLabelY = 148;
+		this.imageHeight = 262;
+		this.inventoryLabelY = 168;
 	}
 
 	@Override
@@ -24,8 +25,16 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 		searchBox.setMaxLength(TerminalMenu.SEARCH_MAX_LENGTH);
 		searchBox.setHint(Component.literal("Search items, @mod, #tag"));
 		searchBox.setResponder(this::onSearchChanged);
+		searchBox.setFocused(false);
 		addRenderableWidget(searchBox);
-		setInitialFocus(searchBox);
+
+		addRenderableWidget(Button.builder(Component.literal("A-Z"), button -> sendMenuButton(TerminalMenu.SORT_NAME_BUTTON))
+				.bounds(this.leftPos + 8, this.topPos + 37, 38, 18)
+				.build());
+
+		addRenderableWidget(Button.builder(Component.literal("Qty"), button -> sendMenuButton(TerminalMenu.SORT_COUNT_BUTTON))
+				.bounds(this.leftPos + 50, this.topPos + 37, 38, 18)
+				.build());
 	}
 
 	@Override
@@ -35,12 +44,13 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 
 		guiGraphics.fill(x, y, x + imageWidth, y + imageHeight, 0xFF2B2118);
 		guiGraphics.fill(x + 4, y + 14, x + imageWidth - 4, y + 34, 0xFF3B3024);
-		guiGraphics.fill(x + 4, y + 34, x + imageWidth - 4, y + 146, 0xFF3B3024);
-		guiGraphics.fill(x + 4, y + 156, x + imageWidth - 4, y + imageHeight - 4, 0xFF3B3024);
+		guiGraphics.fill(x + 4, y + 34, x + imageWidth - 4, y + 56, 0xFF3B3024);
+		guiGraphics.fill(x + 4, y + 56, x + imageWidth - 4, y + 166, 0xFF3B3024);
+		guiGraphics.fill(x + 4, y + 176, x + imageWidth - 4, y + imageHeight - 4, 0xFF3B3024);
 
-		drawSlotBackgrounds(guiGraphics, x + 7, y + 37, TerminalMenu.GRID_COLUMNS, TerminalMenu.GRID_ROWS);
-		drawSlotBackgrounds(guiGraphics, x + 7, y + 159, 9, 3);
-		drawSlotBackgrounds(guiGraphics, x + 7, y + 217, 9, 1);
+		drawSlotBackgrounds(guiGraphics, x + 7, y + 57, TerminalMenu.GRID_COLUMNS, TerminalMenu.GRID_ROWS);
+		drawSlotBackgrounds(guiGraphics, x + 7, y + 179, 9, 3);
+		drawSlotBackgrounds(guiGraphics, x + 7, y + 237, 9, 1);
 	}
 
 	@Override
@@ -53,13 +63,23 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 	protected void renderLabels(GuiGraphics guiGraphics, int mouseX, int mouseY) {
 		guiGraphics.drawString(this.font, this.title, this.titleLabelX, this.titleLabelY, 0xE8D9B5, false);
 		guiGraphics.drawString(this.font, this.playerInventoryTitle, this.inventoryLabelX, this.inventoryLabelY, 0xE8D9B5, false);
-		guiGraphics.drawString(this.font, Component.literal("LMB: stack to cursor  RMB: half  Shift: move stack"), 8, 138, 0xC8B89A, false);
+		guiGraphics.drawString(this.font, Component.literal("LMB: stack to cursor  RMB: half  Shift: move stack"), 8, 158, 0xC8B89A, false);
+	}
+
+	@Override
+	public boolean mouseClicked(double mouseX, double mouseY, int button) {
+		if (searchBox != null && !isMouseOverSearchBox(mouseX, mouseY)) {
+			searchBox.setFocused(false);
+		}
+
+		return super.mouseClicked(mouseX, mouseY, button);
 	}
 
 	@Override
 	public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
 		if (searchBox != null && searchBox.isFocused()) {
 			if (this.minecraft != null && this.minecraft.options.keyInventory.matches(keyCode, scanCode)) {
+				searchBox.setFocused(false);
 				return true;
 			}
 
@@ -73,7 +93,7 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 
 	@Override
 	public boolean charTyped(char codePoint, int modifiers) {
-		if (searchBox != null && searchBox.charTyped(codePoint, modifiers)) {
+		if (searchBox != null && searchBox.isFocused() && searchBox.charTyped(codePoint, modifiers)) {
 			return true;
 		}
 
@@ -91,6 +111,16 @@ public class TerminalScreen extends AbstractContainerScreen<TerminalMenu> {
 			this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, TerminalMenu.SEARCH_CHAR_BASE_BUTTON + character);
 		}
 		this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, TerminalMenu.SEARCH_APPLY_BUTTON);
+	}
+
+	private void sendMenuButton(int buttonId) {
+		if (this.minecraft != null && this.minecraft.gameMode != null) {
+			this.minecraft.gameMode.handleInventoryButtonClick(this.menu.containerId, buttonId);
+		}
+	}
+
+	private boolean isMouseOverSearchBox(double mouseX, double mouseY) {
+		return searchBox != null && mouseX >= searchBox.getX() && mouseX < searchBox.getX() + searchBox.getWidth() && mouseY >= searchBox.getY() && mouseY < searchBox.getY() + searchBox.getHeight();
 	}
 
 	private void drawSlotBackgrounds(GuiGraphics guiGraphics, int startX, int startY, int columns, int rows) {
