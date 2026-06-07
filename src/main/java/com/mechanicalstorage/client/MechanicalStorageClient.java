c4db62859acae850debc5f7357a04336a2f0651e
ALATON18 <littlexdoesminecraft@gmail.com>
2026-06-07 23:10:22 +0100

    Update MechanicalStorageClient.java

--- src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
+++ src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
@@ -1,9 +1,11 @@
 package com.mechanicalstorage.client;
 
 import com.mechanicalstorage.MechanicalStorage;
+import com.simibubi.create.content.kinetics.base.ShaftRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 @EventBusSubscriber(modid = MechanicalStorage.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -11,5 +13,11 @@ public class MechanicalStorageClient {
 	@SubscribeEvent
 	public static void registerMenuScreens(RegisterMenuScreensEvent event) {
 		event.register(MechanicalStorage.TERMINAL_MENU.get(), TerminalScreen::new);
+	}
+
+	@SubscribeEvent
+	public static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_CONNECTOR_BLOCK_ENTITY.get(), ShaftRenderer::new);
+		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_TERMINAL_BLOCK_ENTITY.get(), ShaftRenderer::new);
 	}
 }
