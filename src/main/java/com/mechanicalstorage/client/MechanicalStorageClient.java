dc1b0e394a729ba82b951019f8ef2c8413313f77
ALATON18 <littlexdoesminecraft@gmail.com>
2026-06-09 00:49:42 +0100

    Register client events without deprecated subscriber bus

--- src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
+++ src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
@@ -2,21 +2,24 @@ package com.mechanicalstorage.client;
 
 import com.mechanicalstorage.MechanicalStorage;
 import com.simibubi.create.content.kinetics.base.ShaftRenderer;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
-@EventBusSubscriber(modid = MechanicalStorage.MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-public class MechanicalStorageClient {
-	@SubscribeEvent
-	public static void registerMenuScreens(RegisterMenuScreensEvent event) {
+public final class MechanicalStorageClient {
+	private MechanicalStorageClient() {
+	}
+
+	public static void register(IEventBus modEventBus) {
+		modEventBus.addListener(MechanicalStorageClient::registerMenuScreens);
+		modEventBus.addListener(MechanicalStorageClient::registerBlockEntityRenderers);
+	}
+
+	private static void registerMenuScreens(RegisterMenuScreensEvent event) {
 		event.register(MechanicalStorage.TERMINAL_MENU.get(), TerminalScreen::new);
 	}
 
-	@SubscribeEvent
-	public static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+	private static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
 		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_CONNECTOR_BLOCK_ENTITY.get(), ShaftRenderer::new);
 		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_TERMINAL_BLOCK_ENTITY.get(), ShaftRenderer::new);
 	}
