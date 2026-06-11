045b3a6314b56ccabbdd239feb31ec810a4b27d5
ALATON18 <littlexdoesminecraft@gmail.com>
2026-06-11 01:31:23 +0100

    Let Registrate own machine renderer registration

--- src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
+++ src/main/java/com/mechanicalstorage/client/MechanicalStorageClient.java
@@ -2,7 +2,6 @@ package com.mechanicalstorage.client;
 
 import com.mechanicalstorage.MechanicalStorage;
 import net.neoforged.bus.api.IEventBus;
-import net.neoforged.neoforge.client.event.EntityRenderersEvent;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 
 public final class MechanicalStorageClient {
@@ -11,15 +10,9 @@ public final class MechanicalStorageClient {
 
 	public static void register(IEventBus modEventBus) {
 		modEventBus.addListener(MechanicalStorageClient::registerMenuScreens);
-		modEventBus.addListener(MechanicalStorageClient::registerBlockEntityRenderers);
 	}
 
 	private static void registerMenuScreens(RegisterMenuScreensEvent event) {
 		event.register(MechanicalStorage.TERMINAL_MENU.get(), TerminalScreen::new);
-	}
-
-	private static void registerBlockEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_CONNECTOR_BLOCK_ENTITY.get(), MechanicalStorageShaftRenderer::new);
-		event.registerBlockEntityRenderer(MechanicalStorage.MECHANICAL_STORAGE_TERMINAL_BLOCK_ENTITY.get(), MechanicalStorageShaftRenderer::new);
 	}
 }
