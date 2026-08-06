ea8905558b93a7faaf14a7cae19a4d045bcf92b7
ALATON18 <littlexdoesminecraft@gmail.com>
2026-08-06 19:40:17 +0100

    Expose JEI search runtime

--- src/main/java/com/mechanicalstorage/compat/jei/MechanicalStorageJeiPlugin.java
+++ src/main/java/com/mechanicalstorage/compat/jei/MechanicalStorageJeiPlugin.java
@@ -4,6 +4,8 @@ import com.mechanicalstorage.MechanicalStorage;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IExtraIngredientRegistration;
+import mezz.jei.api.runtime.IIngredientFilter;
+import mezz.jei.api.runtime.IJeiRuntime;
 import net.minecraft.resources.ResourceLocation;
 
 import java.util.List;
@@ -11,6 +13,7 @@ import java.util.List;
 @JeiPlugin
 public class MechanicalStorageJeiPlugin implements IModPlugin {
 	private static final ResourceLocation PLUGIN_ID = ResourceLocation.fromNamespaceAndPath(MechanicalStorage.MODID, "jei_plugin");
+	private IJeiRuntime runtime;
 
 	@Override
 	public ResourceLocation getPluginUid() {
@@ -23,5 +26,30 @@ public class MechanicalStorageJeiPlugin implements IModPlugin {
 				MechanicalStorage.MECHANICAL_STORAGE_CONNECTOR.get().asItem().getDefaultInstance(),
 				MechanicalStorage.MECHANICAL_STORAGE_TERMINAL.get().asItem().getDefaultInstance()
 		));
+	}
+
+	@Override
+	public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+		runtime = jeiRuntime;
+		JeiSearchBridge.connect(this::getFilterText, this::setFilterText);
+	}
+
+	@Override
+	public void onRuntimeUnavailable() {
+		JeiSearchBridge.disconnect();
+		runtime = null;
+	}
+
+	private String getFilterText() {
+		return runtime == null ? "" : runtime.getIngredientFilter().getFilterText();
+	}
+
+	private void setFilterText(String filterText) {
+		if (runtime != null) {
+			IIngredientFilter ingredientFilter = runtime.getIngredientFilter();
+			if (!ingredientFilter.getFilterText().equals(filterText)) {
+				ingredientFilter.setFilterText(filterText);
+			}
+		}
 	}
 }
