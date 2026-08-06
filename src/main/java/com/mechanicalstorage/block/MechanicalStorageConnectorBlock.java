323640456f3aedc8e7880936434d2a73846e9413
ALATON18 <littlexdoesminecraft@gmail.com>
2026-08-06 19:40:29 +0100

    Use shared connector shaft rule

--- src/main/java/com/mechanicalstorage/block/MechanicalStorageConnectorBlock.java
+++ src/main/java/com/mechanicalstorage/block/MechanicalStorageConnectorBlock.java
@@ -3,14 +3,12 @@ package com.mechanicalstorage.block;
 import com.mechanicalstorage.blockentity.MechanicalStorageConnectorBlockEntity;
 import com.simibubi.create.foundation.blockEntity.SmartBlockEntityTicker;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
@@ -35,18 +33,6 @@ public class MechanicalStorageConnectorBlock extends DirectionalMachineBlock imp
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> blockEntityType) {
         return new SmartBlockEntityTicker<>();
     }
-
-    // --- NEW: Proper Create kinetic shaft logic ---
-    @Override
-    public boolean hasShaftTowards(LevelReader world, BlockPos pos, BlockState state, Direction face) {
-        return face == state.getValue(FACING).getOpposite();
-    }
-
-    @Override
-    public Direction.Axis getRotationAxis(BlockState state) {
-        return state.getValue(FACING).getOpposite().getAxis();
-    }
-    // ------------------------------------------------
 
     @Override
     protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
