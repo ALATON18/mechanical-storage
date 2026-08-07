7a18db843a7815716d6b4e0c67f9353f67ee114f
ALATON18 <littlexdoesminecraft@gmail.com>
2026-08-07 02:13:33 +0100

    Render rear shafts through vanilla BER path

--- src/main/java/com/mechanicalstorage/client/MechanicalStorageShaftRenderer.java
+++ src/main/java/com/mechanicalstorage/client/MechanicalStorageShaftRenderer.java
@@ -10,21 +10,25 @@ import net.createmod.catnip.render.SuperByteBuffer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.core.Direction;
 
-public class MechanicalStorageShaftRenderer<T extends KineticBlockEntity> extends KineticBlockEntityRenderer<T> {
+public class MechanicalStorageShaftRenderer<T extends KineticBlockEntity> implements BlockEntityRenderer<T> {
 	public MechanicalStorageShaftRenderer(BlockEntityRendererProvider.Context context) {
-		super(context);
 	}
 
 	@Override
-	protected void renderSafe(T be, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int light, int overlay) {
+	public void render(T be, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int light, int overlay) {
+		if (!be.hasLevel()) {
+			return;
+		}
+
 		Direction front = be.getBlockState().getValue(DirectionalMachineBlock.FACING);
 		Direction shaftDirection = front.getOpposite();
 		SuperByteBuffer shaft = CachedBuffers.partialFacing(AllPartialModels.SHAFT_HALF, be.getBlockState(), shaftDirection);
 		int rearLight = LevelRenderer.getLightColor(be.getLevel(), be.getBlockPos().relative(shaftDirection));
-		standardKineticRotationTransform(shaft, be, rearLight)
-				.renderInto(poseStack, buffer.getBuffer(RenderType.solid()));
+		KineticBlockEntityRenderer.standardKineticRotationTransform(shaft, be, rearLight)
+				.renderInto(poseStack, buffer.getBuffer(RenderType.cutoutMipped()));
 	}
 }
