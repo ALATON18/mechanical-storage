e14d019dc2c50261aa2c199f1b6aa5c2c61d6f68
ALATON18 <littlexdoesminecraft@gmail.com>
2026-08-11 20:18:22 +0100

    Fix connector endpoint level import

--- src/main/java/com/mechanicalstorage/blockentity/MechanicalStorageConnectorBlockEntity.java
+++ src/main/java/com/mechanicalstorage/blockentity/MechanicalStorageConnectorBlockEntity.java
@@ -8,6 +8,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
